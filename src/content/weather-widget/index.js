cb877b75a9394a591509a24d8d8ccb99ac66cb5c
NataliaPavliuk <natalia.pavliuk001@gmail.com>
2021-05-20 13:36:52 +0300

    lab5-6

---
+++ src/content/weather-widget/index.js
@@ -0,0 +1,46 @@
+import API_KEY from '../weather-widget/config';
+import Handler from './modules/handler';
+import UI from './modules/ui';
+
+let metric = true;
+
+$(document).ready(function(){
+  UI.updateWeather(metric);
+
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(success, error);
+  } else {
+    error();
+  }
+
+  $("#fahrenheit-now, #celsius-now").on("click", function(){
+    if($(this).hasClass("degree-inactive")){
+      $(this).removeClass("degree-inactive");
+      $(".degree-active").removeClass("degree-active").addClass("degree-inactive");
+      $(this).addClass("degree-active");
+
+      if($(this).is("#fahrenheit-now")) {
+        metric = false;
+      } else {
+        metric = true;
+      }
+
+      UI.updateWeather(metric);
+    }
+  });
+});
+
+function error() {
+  alert("Sorry, no position available.");
+}
+
+function success(position) {
+  handleRequest(position.coords.latitude, position.coords.longitude);
+}
+
+function handleRequest(latitude, longitude){
+  Handler.fetchCurrentWeather(latitude, longitude, metric);
+  Handler.fetchForecast(latitude, longitude, metric);
+}
+
+export default metric;
